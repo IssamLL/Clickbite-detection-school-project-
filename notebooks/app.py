1e4b7dcbddaa2d96d192e03218116bba21e2dd95
IssamL <issamechaouii@gmail.com>
2024-01-03 16:32:00 +0100

    Update app.py with clickbait classification functionality

--- notebooks/app.py
+++ notebooks/app.py
@@ -14,15 +14,26 @@ model = load_model('model.h5')
 # On subsequent interactions, the cached model will be used
 
 # When the input changes, the cached model will be used
-text = st.text_input("Enter some text", value="Classify this")
+st.header('Clickbait Binary Classifier Develepped')
+st.write(' by ECH-CHAOUI Issam and EL BHIRI Zakariae')
+st.write('This app is based on a LSTM model')
+st.write('The model is trained on the Clickbait dataset')
+st.write('The model is trained on the Clickbait dataset')
+
+text = st.text_input("Enter some text")
+
+
+# * optional kwarg unsafe_allow_html = True
 if text:
-    new_title = list(text)
-    new_sequences = tokenizer.texts_to_sequences(new_title)
-    new_padded_sequences = pad_sequences(new_sequences, maxlen=12, padding='post')
-    output = model.predict(new_padded_sequences)
-    if len(output) > 1:
-        st.write(output)
-        if model.predict(output)[0][0] > 0.5:
-           st.write("This is a postive title")
-        else:
-            st.write("This is a negative tile")
+    title = [text]
+
+    sequences = tokenizer.texts_to_sequences(title)
+    padded_sequences = pad_sequences(sequences, maxlen=12, padding='post')
+    output = model.predict(padded_sequences)
+
+    
+    st.write(output)
+    if model.predict(output) > 0.5:
+        st.write("This is not a clickbait title")
+    else:
+        st.write("This is a clickbait tile")
